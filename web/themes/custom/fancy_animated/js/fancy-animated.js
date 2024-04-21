753b334eb53cd11bac8fa9e7a305b1cc7a15703d
Ahmed Raza <raza1778@gmail.com>
2024-04-21 23:56:49 +0500

    Custom theme

---
+++ web/themes/custom/fancy_animated/js/fancy-animated.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Fancy Animated behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.fancyAnimated = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
